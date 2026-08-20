6a1d14ee1092c2359b4b8e14c2d437711de5e6db
Internetezoo <123302216+Internetezoo@users.noreply.github.com>
2026-08-20 17:49:55 +0200

    Add files via upload

---
+++ api/test_mvh.js
@@ -0,0 +1,41 @@
+﻿export default async function handler(req, res) {
+  // A WMTS URL a query paraméterekkel
+  const targetUrl = "https://mepar.mvh.allamkincstar.gov.hu/api/proxy/iier-gs/gwc/service/wmts?viewparams=VONEV:null;IGDAT:null&SRS=EPSG:23700&layer=iier:topo10&style=raster&tilematrixset=EOV_teszt&Service=WMTS&Request=GetTile&Version=1.0.0&Format=image/png&TileMatrix=EOV_teszt:4&TileCol=4&TileRow=7";
+
+  // A sütik (Cookies) és tokenek stringként
+  const cookieString = [
+    "ACCESS_TOKEN=ide_masold_be_az_access_tokent",
+    "REFRESH_TOKEN=ide_masold_be_a_refresh_tokent",
+    "CSRF_TOKEN=q0Xe3BsWW9PkT8qMRqJxmpBor35L8gv0"
+  ].join("; ");
+
+  try {
+    // Küldjük a kérést a Vercel szerveréről a MVH felé (a beépített fetch-csel)
+    const response = await fetch(targetUrl, {
+      method: 'GET',
+      headers: {
+        "accept": "application/json, text/plain, */*",
+        "accept-language": "hu-HU,hu;q=0.9,en-US;q=0.8,en;q=0.7",
+        "referer": "https://mepar.mvh.allamkincstar.gov.hu/",
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/151.0.0.0 Safari/537.36",
+        "cookie": cookieString,
+        "x-csrf-token": "q0Xe3BsWW9PkT8qMRqJxmpBor35L8gv0"
+      }
+    });
+
+    if (!response.ok) {
+      const errorText = await response.text();
+      return res.status(response.status).send(`MVH Hiba: ${errorText}`);
+    }
+
+    // Ha sikeres, átküldjük a képet a böngésződnek
+    const arrayBuffer = await response.arrayBuffer();
+    const buffer = Buffer.from(arrayBuffer);
+
+    res.setHeader('Content-Type', 'image/png');
+    return res.status(200).send(buffer);
+
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+}
